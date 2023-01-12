bb64a406f27236a4ffaa0d9da8cc3d789eef7207
RachaelKivuti <kivutingatha@gmail.com>
2023-01-12 23:51:18 +0300

    weighted score

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -4,12 +4,12 @@
 DROP procedure IF EXISTS ComputeAverageWeightedScoreForUser;
 DELIMITER |
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser (
-	IN user_id INT
+IN user_id INT
 )
 BEGIN
     UPDATE users
-   	SET average_score=(SELECT AVG(score) FROM corrections
-			     WHERE corrections.user_id=user_id)
-	WHERE id=user_id;
+   SET average_score=(SELECT AVG(score) FROM corrections
+     WHERE corrections.user_id=user_id)
+WHERE id=user_id;
 END;
 |
